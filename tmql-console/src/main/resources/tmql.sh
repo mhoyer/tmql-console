5042edb526dd213b6b4ca7d72e86ec0f6eae21ef
hoyer <mhoyer@pixelplastic.de>
2011-03-23 16:36:50 +0100

    * fixed shell script to allow call from different folder

--- tmql-console/src/main/resources/tmql.sh
+++ tmql-console/src/main/resources/tmql.sh
@@ -3,5 +3,6 @@ if [[ ! -d $JAVA_HOME ]] ;
 then
   echo JAVA_HOME not defined or does not exist!
 else
-  "${JAVA_HOME}/bin/java" -Djava.ext.dirs=./libs -jar TMQLConsole.jar $*
+  SCRIPT_LOCATION=`dirname "$0"`
+  "${JAVA_HOME}/bin/java" -Djava.ext.dirs=$SCRIPT_LOCATION/libs -jar $SCRIPT_LOCATION/TMQLConsole.jar $*
 fi
